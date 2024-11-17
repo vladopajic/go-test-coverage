91a55debe3229361f742f5cd3192977ec143167b
vladopajic <vladopajic@users.noreply.github.com>
2024-11-17 18:47:14 +0100

    add path test (#122)

---
+++ pkg/testcoverage/path/path_test.go
@@ -0,0 +1,30 @@
+package path_test
+
+import (
+	"runtime"
+	"testing"
+
+	. "github.com/vladopajic/go-test-coverage/v2/pkg/testcoverage/path"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_NormalizeForOS(t *testing.T) {
+	t.Parallel()
+
+	if runtime.GOOS == "windows" {
+		assert.Equal(t, "foo\\bar", NormalizeForOS("foo/bar"))
+	} else {
+		assert.Equal(t, "foo/bar", NormalizeForOS("foo/bar"))
+	}
+}
+
+func Test_NormalizeForTool(t *testing.T) {
+	t.Parallel()
+
+	if runtime.GOOS == "windows" {
+		assert.Equal(t, "foo/bar", NormalizeForTool("foo\\bar"))
+	} else {
+		assert.Equal(t, "foo/bar", NormalizeForTool("foo/bar"))
+	}
+}
