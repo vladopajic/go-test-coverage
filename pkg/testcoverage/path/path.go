89e9312bfc668743bc2831a65d0eb50cc0f72276
Vlado Pajić <vlado@brrrrr.at>
2024-11-17 15:15:14 +0100

    unused code cleanup

--- pkg/testcoverage/path/path.go
+++ pkg/testcoverage/path/path.go
@@ -8,14 +8,6 @@ import (
 
 const separatorToReplace = string(filepath.Separator)
 
-// func NormalizeForOS(path string) string {
-// 	if runtime.GOOS != "windows" {
-// 		return path
-// 	}
-
-// 	return strings.ReplaceAll(path, "/", separatorToReplace)
-// }
-
 func NormalizeForTool(path string) string {
 	if runtime.GOOS != "windows" {
 		return path
