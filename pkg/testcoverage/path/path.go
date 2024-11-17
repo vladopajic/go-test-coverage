8862ae8e239195063cf71c89f5013f9513d4ea7e
Vlado Pajić <vlado@brrrrr.at>
2024-11-17 15:25:36 +0100

    fix test

--- pkg/testcoverage/path/path.go
+++ pkg/testcoverage/path/path.go
@@ -8,6 +8,14 @@ import (
 
 const separatorToReplace = string(filepath.Separator)
 
+func NormalizeForOS(path string) string {
+	if runtime.GOOS != "windows" {
+		return path
+	}
+
+	return strings.ReplaceAll(path, "/", separatorToReplace)
+}
+
 func NormalizeForTool(path string) string {
 	if runtime.GOOS != "windows" {
 		return path
