7958f2dd3f19274de73f65905657d2c1406dae06
Vlado Pajić <vlado@brrrrr.at>
2025-05-15 11:00:42 +0200

    fix

--- pkg/testcoverage/coverage/module_test.go
+++ pkg/testcoverage/coverage/module_test.go
@@ -6,11 +6,12 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	. "github.com/vladopajic/go-test-coverage/v2/pkg/testcoverage/coverage"
+	"github.com/vladopajic/go-test-coverage/v2/pkg/testcoverage/path"
 )
 
 func Test_FindGoModFile(t *testing.T) {
 	t.Parallel()
 
 	assert.Empty(t, FindGoModFile(""))
-	assert.Equal(t, "../../../go.mod", FindGoModFile("../../../"))
+	assert.Equal(t, path.NormalizeForTool("../../../go.mod"), FindGoModFile("../../../"))
 }
