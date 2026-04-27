c749b6cbdf1e4e5aee11310d01686f7c1fbda635
Vlado Pajić <vlado@brrrrr.at>
2026-04-27 09:37:49 +0200

    lint fix

--- pkg/testcoverage/coverage/export_test.go
+++ pkg/testcoverage/coverage/export_test.go
@@ -1,18 +1,20 @@
 package coverage
 
 var (
-	FindFileCreator             = findFileCreator
-	FindAnnotations             = findAnnotations
-	FindFuncsAndBlocks          = findFuncsAndBlocks
-	ParseProfiles               = parseProfiles
-	SumCoverage                 = sumCoverage
-	FindGoModFile               = findGoModFile
-	PluckStartLine              = pluckStartLine
+	FindFileCreator            = findFileCreator
+	FindAnnotations            = findAnnotations
+	FindFuncsAndBlocks         = findFuncsAndBlocks
+	ParseProfiles              = parseProfiles
+	SumCoverage                = sumCoverage
+	FindGoModFile              = findGoModFile
+	PluckStartLine             = pluckStartLine
 	FindFilePathMatchingSearch = findFilePathMatchingSearch
 )
 
-type Extent = extent
-type FileInfo = fileInfo
+type (
+	Extent   = extent
+	FileInfo = fileInfo
+)
 
 func NewFileInfo(name string) fileInfo {
 	return fileInfo{name: name}
